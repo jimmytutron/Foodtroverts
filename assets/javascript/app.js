782d3fe3d2507e69821a5631f9a4e2b727d4b034
Okens <ng.eric314@gmail.com>
2018-05-10 16:57:00 -0700

    Enabling API for testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,20 +5,26 @@ $(document).ready(function () {
     // event.preventDefault();
 
 
-    // var authKey = "AIzaSyAZPAsF-Fb-C5lnhtkitRLjplX24zRkqeE";
-    // var city = "San Francisco";
-    // var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+" + city + "&key=" + authKey;
+    var authKey = "AIzaSyAZPAsF-Fb-C5lnhtkitRLjplX24zRkqeE";
+    var city = "San Francisco";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+" + city + "&key=" + authKey;
 
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET",
-    //     dataType: 'jsonp',
-    //     cache: false
-    // }).then(function (response) {
-    //     console.log(response);
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        dataType: 'jsonp',
+        cache: false
+    }).then(function (response) {
+        console.log(response);
 
-    // });
+    });
 
+
+
+
+
+
+    
 //=====Section below for ipstack is working.=======/
     // $.get("http://api.ipstack.com/check?access_key=30d7443ccd61f6cddd884e4525271361", function (res) {
     //     userLocationInfo = res;
