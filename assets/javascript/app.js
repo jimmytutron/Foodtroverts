8c827b375da896f74ba55fd52f831f7995b9f0ff
Okens <ng.eric314@gmail.com>
2018-05-11 13:28:43 -0700

    Updating app.js firebase connection with object and testing inputs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,57 @@
 $(document).ready(function () {
-//=========================GLOBAL===============================//
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDQhnkQVQxYFIzpHHe3f9j46KYowZpgGRg",
-    authDomain: "foodtroverts-1525972295801.firebaseapp.com",
-    databaseURL: "https://foodtroverts-1525972295801.firebaseio.com",
-    projectId: "foodtroverts-1525972295801",
-    storageBucket: "",
-    messagingSenderId: "945445095089"
-  };
-  firebase.initializeApp(config);
+    //=========================GLOBAL===============================//
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDQhnkQVQxYFIzpHHe3f9j46KYowZpgGRg",
+        authDomain: "foodtroverts-1525972295801.firebaseapp.com",
+        databaseURL: "https://foodtroverts-1525972295801.firebaseio.com",
+        projectId: "foodtroverts-1525972295801",
+        storageBucket: "",
+        messagingSenderId: "945445095089"
+    };
+    firebase.initializeApp(config);
 
-  var database = firebase.database();
+    var database = firebase.database();
 
-  var connectionsRef = database.ref("/connections");
-  var connectedRef = database.ref(".info/connected");
+    var connectionsRef = database.ref("/connections");
+    var connectedRef = database.ref(".info/connected");
 
-  connectedRef.on("value", function(snap){
-    if (snap.val()){
-        var userObj = {};
-        var currUserNumb = 1;
+    connectedRef.on("value", function (snap) {
+        if (snap.val()) {
+            var userObj = {};
 
-        userObj.userNumb = currUserNumb;
+            $("#submitPref").on("click", function (event) {
+                event.preventDefault();
+                
+                var userName = $("#userName").val().trim();
+                var userFoodPref = $("#foodPref").val().trim();
+                var userPrefTime = $("#timePref").val().trim();
+                console.log(userName);
+                console.log(userFoodPref);
+                console.log(userPrefTime);
 
-
-
-
-
-//=====IPStack=======/
-    $.get("http://api.ipstack.com/check?access_key=30d7443ccd61f6cddd884e4525271361", function (res) {
-        userLocationInfo = res;
-        // console.log("userLocationInfo:", userLocationInfo)
-        currentCity = res["city"];
-        userObj.location = currentCity;
-        // console.log(currentCity);
-        var con = connectionsRef.push(userObj);
-
-
-//=====Google Places=======/
-
-    var authKey = "AIzaSyAZPAsF-Fb-C5lnhtkitRLjplX24zRkqeE";
-    var city = currentCity;
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+" + city + "&key=" + authKey;
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        // console.log("Place ID: " + response.results[0].place_id);
-    });
+                //=====IPStack=======/
+                $.get("http://api.ipstack.com/check?access_key=30d7443ccd61f6cddd884e4525271361", function (res) {
+                    userLocationInfo = res;
+                    // console.log("userLocationInfo:", userLocationInfo)
+                    currentCity = res["city"];
+                    userObj.location = currentCity;
+                    // console.log(currentCity);
+                    var con = connectionsRef.push(userObj);
 
 
+                    //=====Google Places=======/
 
+                    var authKey = "AIzaSyAZPAsF-Fb-C5lnhtkitRLjplX24zRkqeE";
+                    var city = currentCity;
+                    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+" + city + "&key=" + authKey;
 
-
-
-
-
-
-
-
-    
-
-
-
-
-
-    //=====================GLOBAL (END)=============================//
-    //=========================Jimmy===============================//
-
-
-
-
-
-
-    //=====================Jimmy (END)=============================//
-    //=========================Eric===============================//
-
-
-
-
-
-
-    //=====================Eric (END)=============================//
-    //=========================Lena===============================//
+                    $.ajax({
+                        url: queryURL,
+                        method: "GET"
+                    }).then(function (response) {
+                        // console.log("Place ID: " + response.results[0].place_id);
+                    });
 
 
 
@@ -91,9 +59,39 @@ $(document).ready(function () {
 
 
 
-    //=====================Lena (END)=============================//
-            con.onDisconnect().remove();
+
+
+
+
+                    //=====================GLOBAL (END)=============================//
+                    //=========================Jimmy===============================//
+
+
+
+
+
+
+                    //=====================Jimmy (END)=============================//
+                    //=========================Eric===============================//
+
+
+
+
+
+
+                    //=====================Eric (END)=============================//
+                    //=========================Lena===============================//
+
+
+
+
+
+
+
+                    //=====================Lena (END)=============================//
+                    con.onDisconnect().remove();
                 }, "jsonp");
-    }
-  });
+            });
+        }
+    });
 });
